8a5d299140c0ac0835939bdd368880a1cfac872b
Jannelle <zcemjof@ucl.ac.uk>
2025-10-24 15:00:35 +0100

    Add pytest to confirm single interval behaves as expected

---
+++ test_times.py
@@ -0,0 +1,6 @@
+import pytest
+from times import time_range, compute_overlap_time
+
+def test_time_range_single_interval():
+    result = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")
+    assert result == [("2010-01-12 10:00:00", "2010-01-12 11:00:00")]
